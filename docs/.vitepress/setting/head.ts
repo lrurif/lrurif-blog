6e00cc0d2371416b064631754d23564dfb089289
lrurif <1984811813@qq.com>
2023-03-14 15:41:54 +0800

    调整

--- docs/.vitepress/setting/head.ts
+++ docs/.vitepress/setting/head.ts
@@ -1,5 +1,5 @@
 import type { HeadConfig } from 'vitepress'
-import BASE_URL from "../config"
+import {BASE_URL} from "../config"
 
 export const head: HeadConfig[] = [
   ['link', { rel: 'icon', href: `${BASE_URL}favicon.ico` }],
